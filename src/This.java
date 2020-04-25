1298f2452b13122b093ad7de647d8e2ab316ed2f
dearestman <Stupakov.dmitriy1997@gmail.com>
2020-04-25 16:48:24 +0300

    reloading methods in constructor

--- src/This.java
+++ src/This.java
@@ -1,14 +1,32 @@
 public class This {
     public static void main(String[] args) {
-        Human human1 = new Human();
-        human1.setAge(18);
-        human1.setName("Dima");
+        Human human1 = new Human("Dima", 22);
+        Human human2 = new Human();
         human1.getInfo();
+        human2.getInfo();
+
     }
 }
 class Human{
+
+    public Human(){
+        this.name = "Имя по умолчанию";
+        this.age = 0;
+    }
+
     String name;
     int age;
+
+    public Human(String name, int age){
+        if (name!="" && age>=0){
+            this.name = name;
+            this.age = age;
+        } else {
+            System.out.println("Не правильно указан возвраст или имя!!!");
+        }
+
+    }
+
 
     public void setName(String name){
         this.name=name;
@@ -26,7 +44,7 @@ class Human{
     }
 
     public void getInfo(){
-        System.out.println("name: " + name + " age: " + age);
+        System.out.println("name: " + name + ", age: " + age);
     }
 
 }
