e0cb5173fd497d6e862e86ce021ef5d8009e0295
dearestman <Stupakov.dmitriy1997@gmail.com>
2020-04-25 14:38:21 +0300

    Methods in clesses

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,17 +3,30 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Роман";
         person1.age = 50;
-        System.out.println("Меня зовут " + person1.name + ". И мне уже " + person1.age);
+        person1.speak();
 
         Person person2 = new Person();
-        person2.name = "Vova";
+        person2.name = "Вова";
         person2.age = 20;
-        System.out.println("Меня зовут " + person2.name + ". И мне уже " + person2.age);
+        person2.speak();
+
+        person1.sayHello(person2);
+
+
 
     }
 }
 class Person{
     String name;
     int age;
+
+    void speak(){
+        for (int i=0; i<3; i++)
+            System.out.println("Меня зовут " + name + ", мне уже " + age + "(");
+    }
+
+    void sayHello(Person person){
+        System.out.println("Привет " + person.name + "! Это " + name + ".");
+    }
 }
 
