df0c0f4b390139b1ee6ea795ca10606b048cd69d
dearestman <Stupakov.dmitriy1997@gmail.com>
2020-04-25 14:58:26 +0300

    Methods in classes part 3

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,13 +1,12 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
+        person1.setNameAndAge("Роман",50);
         person1.speak();
 
+        String s1 = "Вован";
         Person person2 = new Person();
-        person2.name = "Вова";
-        person2.age = 20;
+        person2.setNameAndAge(s1,20);
         person2.speak();
 
         person1.sayHello(person2);
@@ -21,6 +20,11 @@ public class ClassesAndObjects {
 class Person{
     String name;
     int age;
+
+    void setNameAndAge(String username, int userage){
+        name=username;
+        age=userage;
+    }
 
     int calculateYearsToRetirement(){
         return 65-age;
