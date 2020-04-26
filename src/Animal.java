691bd217ba2ef4e948d937c9b6034cc47d6ced54
dearestman <Stupakov.dmitriy1997@gmail.com>
2020-04-26 15:52:59 +0300

    Extending

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    String name = "Some animal";
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+    public void sleep(){
+        System.out.println("I am sleeping");
+    }
+    public void showName(){
+        System.out.println(name );
+    }
+}
