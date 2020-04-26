e99ab9bb500198d84491cf9441b1513bb76caac2
dearestman <Stupakov.dmitriy1997@gmail.com>
2020-04-26 10:48:39 +0300

    formating srings in console (printf)

--- src/Strings.java
+++ src/Strings.java
@@ -1,19 +1,14 @@
 public class Strings {
     public static void main(String[] args) {
-//        String s = "Hello!";
-//        s=s.toUpperCase();
-//        System.out.println(s);
-//
-//        String s1 = "Hello";
-//        String s2 = " my";
-//        String s3 = " friend";
-//        String sAll = s1+s2+s3;
-//        System.out.println(sAll);
-
-        StringBuilder stringBuilder = new StringBuilder("Hello");
-        System.out.println(stringBuilder.toString());
-        stringBuilder.append(" my").append(" friend");
-        System.out.println(stringBuilder.toString());
-
+        System.out.print("hello");
+        System.out.println("hello again");
+        System.out.printf("%f This is a string, %s, %d", 2.32 ,"NICE",21);
+        System.out.println();
+        System.out.printf("String %10d \n", 43);
+        System.out.printf("String %10d \n", 5);
+        System.out.printf("String %10d \n", 434334);
+        System.out.printf("String %10.2f \n", 2.3213);
+        System.out.printf("String %10.2f \n", 322.3213);
+        System.out.printf("String %10.2f \n", 543532.3213);
     }
 }
